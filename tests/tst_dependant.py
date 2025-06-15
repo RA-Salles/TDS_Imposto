083b1889b9f2d451b09f7d69885272fad06bba03
locust <locusttheeater@gmail.com>
2025-06-15 02:45:43 -0300

    FEAT: adding new test for dependents
    Have no foxtrot idea of how the foxtrot i should write this.
    
    Not the code, the word dependant or dependent or dependents. Foxtrot Mike Sideways, my english stinks.

---
+++ tests/tst_dependant.py
@@ -0,0 +1,14 @@
+from unittest import TestCase as tc
+from context import irpf
+import irpf.irpf # and now this should be the CLASS we want to test.
+
+class test_dependant(tc):
+
+    def setUp(self):
+        self.taxer = irpf.irpf.irpf()
+
+    def test_dependant_I(self):
+        self.taxer.register_dependant("John")
+        self.assertEqual(self.taxer.get_deduction(), float(150))
+
+    
