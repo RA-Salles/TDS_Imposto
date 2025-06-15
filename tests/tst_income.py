33fc8d771b02d3b094f053e370db113aecaa894b
locust <locusttheeater@gmail.com>
2025-06-15 02:21:15 -0300

    FEAT: test now works as intended
    it fails perceiving the code has no such method instead of "generic import error number 17280421".
    
    Also, here's three test cases.
    We'll now try to make 'em pass one by one.

--- tests/tst_income.py
+++ tests/tst_income.py
@@ -1,11 +1,22 @@
 from unittest import TestCase as tc
-from context import irpf # and now this should be the CLASS we want to test.
+from context import irpf
+import irpf.irpf # and now this should be the CLASS we want to test.
 
 class test_income(tc):
 
-    def test_register_income_I(self):
-        self.assertEqual(irpf.register_income(800.00), float(800))
+    def setUp(self):
+        self.taxer = irpf.irpf.irpf()
 
     def test_register_income_I(self):
-        self.assertEqual(irpf.register_income(800.00), float(800))
+        self.taxer.register_income(800.00)
+        self.assertEqual(irpf.get_income(), float(800))
+
+    def test_register_income_II(self):
+        self.taxer.register_income(900.00)
+        self.assertEqual(irpf.get_income(), float(900))
+
+    def test_register_income_III(self):
+        self.taxer.register_income(900.00)
+        self.taxer.register_income(800.00)
+        self.assertEqual(irpf.get_income(), float(1700))
 
